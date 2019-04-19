4b1802c24cab5e0cd179ced6c401beb42fcd5b3b
martinadellobuono <34475729+martinadellobuono@users.noreply.github.com>
2019-04-19 20:14:46 +0200

    scroll-bar js correction

--- docs/js/scroll-bar.js
+++ docs/js/scroll-bar.js
@@ -1,11 +1,13 @@
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 var toChange = document.getElementById("example-menu");
+var mobile = document.getElementById("mobile-menu");
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     toChange.style.backgroundColor = "#f8f9fb";
+    mobile.style.backgroundColor = "#f8f9fb";
   } else {
-    toChange.style.backgroundColor = 'rgba(255, 255, 255, 0)';
+    toChange.style.backgroundColor = "rgba(255, 255, 255, 0)";
+    mobile.style.backgroundColor = "rgba(255, 255, 255, 0)";
   }
 }
