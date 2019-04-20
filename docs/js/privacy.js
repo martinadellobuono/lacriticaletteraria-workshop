18a0bf667cd8c3f308d8e10ae2b7a9a7cff26eb6
martinadellobuono <34475729+martinadellobuono@users.noreply.github.com>
2019-04-20 13:23:22 +0200

    new privacy policy js

---
+++ docs/js/privacy.js
@@ -0,0 +1,7 @@
+var privacyInfo = document.getElementById("privacy-info");
+
+function privacy() {
+    privacyInfo.innerHTML = "<h3> Privacy Policy</h3> Your privacy is very important to us. Accordingly, we have developed this Policy in order for you to understand how we collect, use, communicate and disclose and make use of personal information. We will protect personal information by reasonable security safeguards against loss or theft, as well as unauthorized access, disclosure, copying, use or modification."
+    privacyInfo.style.color = "#f8f9fb";
+    privacyInfo.style.textAlign = "justify";
+}
