74ea9505cec5f19963e3fd02260ed3ca5d4e4ce5
martinadellobuono <34475729+martinadellobuono@users.noreply.github.com>
2019-04-27 17:57:12 +0200

    new mail-policy js

---
+++ docs/js/mail-policy.js
@@ -0,0 +1,7 @@
+var mailInfo = document.getElementById("mail-info");
+
+function mailPolicy() {
+    mailInfo.innerHTML = "<h3>Informativa sulla privacy</h3>La tua privacy è molto importante per noi. Di conseguenza, abbiamo sviluppato questa Politica per farvi capire come raccogliamo, usiamo, comunichiamo e divulghiamo e facciamo uso di informazioni personali. Proteggiamo le informazioni personali mediante ragionevoli misure di sicurezza contro la perdita o il furto, nonché l'accesso non autorizzato, la divulgazione, la copia, l'uso o la modifica."
+    mailInfo.style.color = "#323232";
+    mailInfo.style.textAlign = "justify";
+}
