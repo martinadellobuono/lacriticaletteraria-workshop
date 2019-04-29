d9bb8186c1f4f04243ef80d745c216c78214d2c6
martinadellobuono <34475729+martinadellobuono@users.noreply.github.com>
2019-04-29 17:02:35 +0200

    new scroll-bar js

--- docs/js/scroll-bar.js
+++ docs/js/scroll-bar.js
@@ -1,16 +1,15 @@
-/*window.onscroll = function() {scrollFunction()};
-//var mobileToggle = document.getElementById("mobile-toggle");
+window.onscroll = function() {scrollFunction()};
 var menuList = document.getElementById("example-menu");
 var toggleBar = document.getElementById("toggle-bar");
+var header = document.getElementById("list");
+var selectedItem = header.getElementsByClassName("item");
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    //mobileToggle.style.backgroundColor = "#f8f9fb";
     menuList.style.backgroundColor = "#f8f9fb";
     toggleBar.style.backgroundColor = "#f8f9fb";
   } else {
-    //mobileToggle.style.backgroundColor = "rgba(255, 255, 255, 0)";
     menuList.style.backgroundColor = "rgba(255, 255, 255, 0)";
     toggleBar.style.backgroundColor = "rgba(255, 255, 255, 0)";
   }
-} */
+}
